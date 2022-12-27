b8f7832f6d7e2bee4a7bf40b5f44bb6a0b5af274
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2022-12-28 01:44:15 +0530

    updated

---
+++ butterflypt.java
@@ -0,0 +1,41 @@
+public class butterflypt {
+    public static void main(String[] args){
+        int n=5;
+       //upper half
+       for(int i=1; i<=i; i++){
+           //ist part
+           for(int j=1; j<=i; j++){
+               System.out.print("*");
+           }
+           //space
+           int spaces = 2*(n-1);
+           for(int j=1; j<=spaces; j++){
+               System.out.print(" ");
+           }
+           //2nd part
+           for ( int j=1; j<=spaces;j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+       //lower half
+        for(int i=n; i<=1; i--){
+            //ist part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //space
+            int spaces = 2*(n-1);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for ( int j=1; j<=spaces;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}

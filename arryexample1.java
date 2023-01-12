e99d71973dde000de916cdf3a3175180348a287f
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-01-12 19:37:10 +0530

    Array

---
+++ arryexample1.java
@@ -0,0 +1,11 @@
+public class arryexample1 {
+    public static void main( String args []){
+        int marks[] = new int[3];
+        marks[0] =97;
+        marks[1]=98;
+        marks[2]=94;
+        for (int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}

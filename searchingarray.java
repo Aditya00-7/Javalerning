88b3b5b73e6bec8d7c2250666ae99c7f5c6c999f
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-01-13 22:34:07 +0530

    updates

--- searchingarray.java
+++ searchingarray.java
@@ -10,9 +10,12 @@ public class searchingarray {
         for(int i=0; i<size; i++) {
             numbers[i] = sc.nextInt();
 
-            int x =sc.nextInt();
-        } //output
-            for(int i=0;i<numbers.length; i++){
+            
+        } 
+        int x = sc.nextInt();
+        
+        //output
+         for(int i=0;i<numbers.length; i++){
                 if(numbers[i]== x){
                 System.out.println("x found at index:"+ i);
 

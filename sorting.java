cc917766e91a690cd3a0baafa1721aa968bcba06
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-02-25 15:51:24 +0530

    updaates

--- sorting.java
+++ sorting.java
@@ -1,11 +1,11 @@
 public class sorting {
-    public static void printArray(int arr[]) {
+   public static void printArray(int arr[]) {
         for( int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
         System.out.println();
         
-    }
+   }
     public static void main(String args[]){
         int arr[]={7,8,3,1,2};
         //bubble

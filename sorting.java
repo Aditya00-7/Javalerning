ff852f9c662aa867daceb7a18d52bc498b7305d6
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-02-25 15:58:09 +0530

    final

--- sorting.java
+++ sorting.java
@@ -1,24 +1,30 @@
 public class sorting {
-   public static void printArray(int arr[]) {
-        for( int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
-        
-   }
-    public static void main(String args[]){
-        int arr[]={7,8,3,1,2};
-        //bubble
-        for( int i =0; i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1; j++){
-                if (arr[j] >arr[j+1]){
+    }
+
+
+    public static void main(String args[]) {
+        int arr[] = {7, 8, 1, 3, 2};
+
+
+        //bubble sort
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
                     //swap
-                    int temp =arr[i];
-                    arr[j]=arr[j+1];
-                    arr[j+i]=temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
+
+
         printArray(arr);
     }
 }
+

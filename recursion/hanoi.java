f238382583bf3efb1950a08b0095928f1bc313a3
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-02-28 20:25:42 +0530

    updates

---
+++ recursion/hanoi.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class hanoi {
+    public static void main( String args[]){
+        int n=3;
+        towerhanoi(n,"s","H","D");
+    }
+    public static void towerhanoi( int n, String src, String helper, String dest ){
+        if (n==1){
+            System.out.println("transfer disk"+ n +" from "+src+" to "+dest);
+            return;
+        }
+        towerhanoi(n-1,src,helper,dest);
+        System.out.println("transfer disk "+n+" from "+src+" to "+dest);
+        towerhanoi(n-1,helper,src,dest);
+    }
+}

d03b2f50aab5a62a5e6ddacaaaea3b9ef764e8f0
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-04-05 23:10:35 +0530

    day3

--- recursion/keypadcombo.java
+++ recursion/keypadcombo.java
@@ -1,4 +1,21 @@
 package recursion;
+import java.util.HashSet;
 
-public class keypadcombo {
+public class keypadcombo {public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
+        "tu", "vwx", "yz"};
+    public static void printKeypadCombination(String number, int idx, String res) {
+        if(idx == number.length()) {
+            System.out.println(res);
+            return;
+        }
+
+        for(int i=0; i<keypad[number.charAt(idx)-'0'].length(); i++) {
+            char curr = keypad[number.charAt(idx)-'0'].charAt(i);
+            printKeypadCombination(number, idx+1, res+curr);
+        }
+    }
+    public static void main(String args[]) {
+        String number = "44";
+        printKeypadCombination(number, 0, "");
+    }
 }

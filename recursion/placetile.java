a3af9b76f2e3cb47dbd094759b7231a376e76399
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-04-09 15:56:51 +0530

    day 4 3

---
+++ recursion/placetile.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class placetile {
+    public static void main(String args[]) {
+        int n = 4, m = 2;
+        System.out.println(placeTiles(n, m));
+    }
+
+    public static int placeTiles(int n, int m) {
+        if (n < m) {
+            return 1;
+        } else if (n == m) {
+            return 2;
+        }
+        return placeTiles(n - 1, m) + placeTiles(n - m, m);
+    }
+}

a5c7ee7ca611b482b153f6cc8a773ee1ae478fe0
Aditya Raj <adityarajbetch@delhitechnicalcampus.ac.in>
2023-04-18 19:57:39 +0530

    opps

--- opps/firstprogram.java
+++ opps/firstprogram.java
@@ -1,28 +1,43 @@
 package opps;
 
 public class firstprogram {
-    class Pen {
+  /*  static class Pen {
         String color;
+        String type;
 
-        public void printColor() {
-            System.out.println("The color of this Pen is " + this.color);
+        public void write() {
+            System.out.println("Writing something");
+        }
+
+        public void printcolor() {
+            System.out.println(this.color);
+        }
+    }*/
+
+    static  class Student {
+        String name;
+        int age;
+
+        public void printinfo() {
+            System.out.println(this.name);
+            System.out.println(this.age);
         }
     }
-    public void main(String args[]) {
-            Pen p1 = new Pen();
-            p1.color = blue;
 
-
-            Pen p2 = new Pen();
-            p2.color = black;
-
-
-            Pen p3 = new Pen();
-            p3.color = red;
-            p1.printColor();
-            p2.printColor();
-            p3.printColor();
-        }
+    public static void main(String[] args) {
+        /*   Pen pen1 = new Pen ();
+           pen1.color="blue";
+           pen1.type ="gel";
+         //  pen1.write();
+        Pen pen2 =new Pen();
+        pen2.color= "red";
+        pen2. type="ballpoint";
+        pen1.printcolor();
+        pen2.printcolor();
+       }*/
+        Student s1 = new Student();
+        s1.name = "aman";
+        s1.age = 24;
+        s1.printinfo();
     }
-
 }
